f61f46de386ded92cc72eecb62cd5c0bb960fa0a
Casey Allen <caseyallen1024@gmail.com>
2022-06-17 13:44:08 +0100

    Just cleared git cache

--- out/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const TravWritable_1 = __importDefault(require("./TravWritable"));
-module.exports = exports = TravWritable_1.default;
-module.exports.TravWritable = TravWritable_1.default;
